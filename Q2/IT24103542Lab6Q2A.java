5f2c01985e6db4a4676e768c4c49818127e5087e
IT24103542 <it24103542@my.sliit.lk>
2024-09-02 17:00:07 +0530

    Lab 6 - Q2

---
+++ Q2/IT24103542Lab6Q2A.java
@@ -0,0 +1,12 @@
+public class IT24103542Lab6Q2A{
+	public static void main(String[] args){
+	int count=1;
+
+	while (count<=10){
+		System.out.print(count);
+		System.out.print(" ");
+		count = count + 1;
+	}
+
+	}
+}
